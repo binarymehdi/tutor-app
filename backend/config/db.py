14c18dfa5cad61c1e280161b2dec470c1ba4c36f
binarymehdi <elmehdilaidoudi@gmail.com>
2023-10-02 15:44:47 +0100

    work on folder structure and backend django apps

---
+++ backend/config/db.py
@@ -0,0 +1,12 @@
+# Using Neon serverless database with Django
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'neondb',
+    'USER': 'binarymehdi',
+    'PASSWORD': 'oynq0rSGIv1A',
+    'HOST': 'ep-aged-band-47562365.eu-central-1.aws.neon.tech',
+    'PORT': '5432',
+  }
+}
