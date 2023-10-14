ab9cc154af1496f37468a0f580ed8a440a562ee7
binarymehdi <elmehdilaidoudi@gmail.com>
2023-10-14 22:46:11 +0100

    more folder structure

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -5,24 +5,23 @@ const Home: React.FC = () => {
     return (
         <div className="flex flex-col h-screen bg-white">
             <Navbar />
-            <div className="flex-1 flex justify-center items-center overflow-y-auto">
-                <div className="grid grid-cols-3 gap-4 w-full max-w-screen-xl mx-auto p-8">
-                    <div className="bg-white p-8 rounded-xl shadow-md col-span-1">
-                        <h1 className="text-2xl mb-4 font-bold text-blue-500">
-                            Tutoring
-                        </h1>
-                        <p className="text-gray-600">
-                            Discover a world of features and experiences tailored just for you. Dive in now.
-                        </p>
-                        <div className="mt-4">
-                            <button className="bg-blue-200 text-blue-700 hover:bg-blue-300 hover:text-blue-800 py-2 px-6 rounded-full transition duration-300 ease-in-out">
-                                Learn More
-                            </button>
-                        </div>
+            <div className="flex-grow flex justify-center items-center">
+                <div className="bg-white p-8 rounded-xl shadow-md max-w-xl text-center">
+                    <h1 className="text-2xl mb-4 font-bold text-blue-500">
+                        Tutoring
+                    </h1>
+                    <p className="text-gray-600">
+                        Make impact now ! Help us provide many students with good quality education
+                    </p>
+                    <div className="mt-4">
+                        <button className="bg-blue-200 text-blue-700 hover:bg-blue-300 hover:text-blue-800 py-2 px-6 rounded-full transition duration-300 ease-in-out">
+                            Apply
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
+
     );
 };
 
