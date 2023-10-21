a86fb7fae23de21c8c9b888c711e6f6202b3e676
134340 <113696378+KarimaTouhami@users.noreply.github.com>
2023-10-21 07:33:19 +0000

    update navbar

--- frontend/src/components/layout/navbar.tsx
+++ frontend/src/components/layout/navbar.tsx
@@ -2,43 +2,27 @@ import React from 'react';
 
 const Navbar: React.FC = () => {
     return (
-        <nav
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '20px',
-                backgroundColor: 'white',
-                boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-                borderRadius: '50px',
-                maxWidth: 'calc(100% - 240px)',
-                margin: '10px 120px', 
-                padding: '15px 30px',
-            }}
-        >
-            <div className="hidden md:flex space-x-4">
-                <NavItem to="/home">Home</NavItem>
-                <NavItem to="/schools">Schools</NavItem>
-                <NavItem to="/contact">Contact</NavItem>
-            </div>
-            <div
-                style={{
-                    fontSize: '2rem',
-                    fontWeight: 'bold',
-                    color: '#64aef2',
-                    flex: 1,
-                    textAlign: 'center',
-                }}
-            >
-                Tutor App
-            </div>
-            <div className="hidden md:flex space-x-4">
-                <NavItem to="/about">About</NavItem>
-                <button className="text-blue-700 hover:text-blue-500 transition duration-300 ease-in-out">
-                    Sign In
-                </button>
-                <button className="bg-blue-500 text-white hover:bg-blue-600 py-2 px-6 rounded-full transition duration-300 ease-in-out">
-                    Sign Up
-                </button>
+        <nav className="bg-blue-900 text-white p-4 shadow-lg">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-2xl font-bold">
+                    <span className="text-blue-300">Tutoring</span> App
+                </div>
+                <ul className="flex space-x-6">
+                    <NavItem to="/home">Home</NavItem>
+                    <NavItem to="/tutoring">Tutoring Services</NavItem>
+                    <NavItem to="/about">About Us</NavItem>
+                    <NavItem to="/how">How It Works</NavItem>
+                    <NavItem to="/faq">FAQ</NavItem>
+                    <NavItem to="/contact">Contact</NavItem>
+                </ul>
+                <div className="flex items-center space-x-4">
+                    <button className="text-blue-300 hover:text-blue-100 transition duration-300 ease-in-out">
+                        Sign In
+                    </button>
+                    <button className="bg-blue-500 text-white hover:bg-blue-600 py-2 px-6 rounded-full transition duration-300 ease-in-out">
+                        Sign Up
+                    </button>
+                </div>
             </div>
         </nav>
     );
@@ -48,7 +32,7 @@ const NavItem: React.FC<{ to: string }> = ({ to, children }) => {
     return (
         <a
             href={to}
-            className="text-blue-700 hover:text-blue-500 transition duration-300 ease-in-out"
+            className="text-blue-300 hover:text-blue-100 transition duration-300 ease-in-out"
         >
             {children}
         </a>
