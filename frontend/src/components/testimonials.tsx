43946a93c5b36a43c9e5819899cd97a4b1ff409c
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-11-06 06:36:49 +0000

    fixed tailwind classes

--- frontend/src/components/testimonials.tsx
+++ frontend/src/components/testimonials.tsx
@@ -2,7 +2,7 @@ function Testimonials() {
   return (
     <section className="relative">
       {/* Illustration behind content */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -mb-32" aria-hidden="true">
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none lg:hidden -mb-32" aria-hidden="true">
         <svg width="1760" height="518" viewBox="0 0 1760 518" xmlns="http://www.w3.org/2000/svg">
           <defs>
             <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-02">
@@ -22,49 +22,34 @@ function Testimonials() {
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h2 className="h2 mb-4">Trusted by over 20,000 companies all over the world</h2>
-            <p className="text-xl text-gray-600" data-aos="zoom-y-out">
-              Arcu cursus vitae congue mauris rhoncus viverra nibh cras pulvinar mattis
-              blandit libero cursus mattis.
+            <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">Trusted by over 20,000 companies all over the world</h2>
+            <p className="text-lg text-gray-600" data-aos="zoom-y-out">
+              Arcu cursus vitae congue mauris rhoncus viverra nibh cras pulvinar mattis blandit libero cursus mattis.
             </p>
           </div>
 
           {/* Items */}
-          <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 grid-cols-4 md:grid-cols-5">
-            {/* Item */}
-            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
-              {/* Your content here */}
-            </div>
-            {/* Item */}
-            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
-              {/* Your content here */}
-            </div>
-            {/* Item */}
-            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
-              {/* Your content here */}
-            </div>
-            {/* Item */}
-            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
-              {/* Your content here */}
-            </div>
-            {/* Item */}
-            <div className="flex items-center justify-center py-2 col-span-2 md:col-auto col-start-2 col-end-4">
-              {/* Your content here */}
-            </div>
+          <div className="max-w-full md:max-w-4xl mx-auto grid gap-4 md:gap-6 grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+            {[...Array(5)].map((_, index) => (
+              <div key={index} className="flex items-center justify-center py-2 md:py-4 col-span-1">
+                {/* Your content here */}
+              </div>
+            ))}
           </div>
 
           {/* Testimonials */}
           <div className="max-w-3xl mx-auto mt-20" data-aos="zoom-y-out">
             <div className="relative flex items-start border-2 border-gray-200 rounded bg-white">
               {/* Testimonial */}
-              <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0">
+              <div className="text-center px-6 md:px-10 py-8 md:py-12 pt-12 mx-4 md:mx-0">
                 <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
+                  {/* Image goes here */}
                 </div>
-                <blockquote className="text-xl font-medium mb-4">
+                <blockquote className="text-lg md:text-xl font-medium mb-6">
                   “ I love this product and would recommend it to anyone. Could not be easier to use, and our multiple websites are wonderful. We get nice comments all the time. “
                 </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">Darya Finger</cite>
-                <div className="text-gray-600">
+                <cite className="block text-base md:text-lg font-bold mb-3">Darya Finger</cite>
+                <div className="text-gray-600 text-sm">
                   <span>CEO & Co-Founder</span> <a className="text-blue-600 hover:underline" href="#0">@Dropbox</a>
                 </div>
               </div>
