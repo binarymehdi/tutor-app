bcd43ac22e4a21a051c06d73b179f2b619be8715
binarymehdi <elmehdilaidoudi@gmail.com>
2023-11-18 21:41:51 +0100

    improving the frontend

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,30 +1,49 @@
-import React from 'react';
-import Navbar from '../components/layout/navbar';  // Ensure the path is correct
-import Footer from '../components/layout/footer';
+// import { useState } from 'react'
+// import { Dialog } from '@headlessui/react'
+// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Header from '../components/header'
 
-const Home: React.FC = () => {
-    return (
-        <div className="flex flex-col h-screen bg-white">
-            <Navbar />
-            <div className="flex-grow flex justify-center items-center">
-                <div className="bg-white p-8 rounded-xl shadow-md max-w-xl text-center">
-                    <h1 className="text-2xl mb-4 font-bold text-blue-500">
-                        Tutoring
-                    </h1>
-                    <p className="text-gray-600">
-                        Make impact now ! Help us provide many students with good quality education
-                    </p>
-                    <div className="mt-4">
-                        <button className="bg-blue-200 text-blue-700 hover:bg-blue-300 hover:text-blue-800 py-2 px-6 rounded-full transition duration-300 ease-in-out">
-                            Apply
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <Footer />
+// const navigation = [
+//   { name: 'Product', href: '#' },
+//   { name: 'Features', href: '#' },
+//   { name: 'Marketplace', href: '#' },
+//   { name: 'Company', href: '#' },
+// ]
+
+export default function Example() {
+  // const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <div className="bg-white">
+      <Header />
+
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+          <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}>
+          </div>
         </div>
+        <div className="mx-auto max-w-5xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-6xl md:text-7xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
+            <p className="mt-6 text-xl leading-8 text-gray-600">
+              Facilitating tutoring activities for students by Al Akhawayn
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a href="#" className="rounded-md bg-indigo-600 px-4 py-3 text-base font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Get started
+              </a>
+              <a href="#" className="text-base font-semibold leading-6 text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+          <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}>
+          </div>
+        </div>
+      </div>
 
-    );
-};
-
-export default Home;
+    </div>
+  )
+}
