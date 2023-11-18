da2db4490d2b63c16db1a0928d724496a202665f
binarymehdi <elmehdilaidoudi@gmail.com>
2023-11-19 00:53:58 +0100

    adding the about page

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,22 +1,9 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
- import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import Header from '../components/header'
+import Features from "../components/features";
 
-const navigation = [
-  { name: 'Product', href: '#' },
- { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
- { name: 'Company', href: '#' },
-]
-
-export default function Example() {
-  // const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+const Home = () => {
 
   return (
     <div className="bg-white">
-     
-
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
           <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}>
@@ -24,9 +11,9 @@ export default function Example() {
         </div>
         <div className="mx-auto max-w-5xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-6xl md:text-7xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
-            <p className="mt-6 text-xl leading-8 text-gray-600">
-              Facilitating tutoring activities for students by Al Akhawayn
+            <h1 className="text-9xl md:text-7xl font-extrabold leading-tighter tracking-tighter mb-4">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
+            <p className="mt-6 text-3xl leading-8 text-gray-600">
+              Facilitating tutoring activities for highschool and middleschool students by Al Akhawayn university students
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a href="#" className="rounded-md bg-indigo-600 px-4 py-3 text-base font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
@@ -42,8 +29,11 @@ export default function Example() {
           <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}>
           </div>
         </div>
-      </div>
-
+        <Features />
+      </div> 
     </div>
+
   )
-}
+};
+
+export default Home;
